a21f8ce1cc111d60665e774c5993a79b998d1002
David Wu <david204code@gmail.com>
2021-03-06 20:03:08 +0000

    Project completed, add eventListener with functionality of classList add/remove/toggle css class

---
+++ app.js
@@ -0,0 +1,19 @@
+// select the HTML document
+const toggleBtn = document.querySelector(".sidebar-toggle");
+const closeBtn = document.querySelector(".close-btn");
+const sidebar = document.querySelector(".sidebar");
+
+// add event listener
+toggleBtn.addEventListener("click", function () {
+  // console.log(sidebar.classList);
+  // if (sidebar.classList.contains("show-sidebar")) {
+  //   sidebar.classList.remove("show-sidebar");
+  // } else {
+  //   sidebar.classList.add("show-sidebar");
+  // }
+  sidebar.classList.toggle("show-sidebar");
+});
+
+closeBtn.addEventListener("click", function () {
+  sidebar.classList.remove("show-sidebar");
+});
